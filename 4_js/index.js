50de8d9b4d17c8b5b29062d510de53ca42f5a16f
remisany <remi.d.sany@gmail.com>
2021-05-18 16:10:49 +0200

    Reconnaissance trois caractères

--- 4_js/index.js
+++ 4_js/index.js
@@ -1,10 +1,41 @@
 const submit = document.querySelector(".main-research__submit");
 
+//Ecoute le click sur la loupe
 submit.addEventListener("click", function () {
-    const search = document.querySelector(".main-research__input").value;
+    const searchClick = document.querySelector(".main-research__input").value;
 
     if (search !== "") {
-        console.log(search);
+        console.log(searchClick);
     }
 });
 
+//Ecoute les touches
+const input = document.querySelector(".main-research__input");
+var searchTyped;
+var searchTypedArray = [];
+const regSearchTyped = /[a-zA-Zéèê_\s]/g;
+
+input.addEventListener("keydown", function (event) {
+
+    if ((event.key.match(regSearchTyped)) && (event.key.length === 1)) {
+        
+        if (searchTyped === undefined) {
+            searchTyped = event.key;
+        } else {
+            searchTyped = searchTyped + event.key;
+        }
+
+        if (searchTyped.length >= 3) {
+            console.log(searchTyped);
+        }
+    }
+
+    if ((event.key === "Backspace") && (searchTyped.length != 0)) {
+        
+        searchTyped = searchTyped.substr(0, searchTyped.length - 1);
+        
+        if (searchTyped.length >= 3) {
+            console.log(searchTyped);
+        }
+    }
+});
